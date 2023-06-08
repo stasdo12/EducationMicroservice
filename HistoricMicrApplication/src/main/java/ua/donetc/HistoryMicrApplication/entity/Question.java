4a814f5f55c40eb8a824352af135a6a63177d32e
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-06-08 13:36:16 +0300

    import optimisation

--- HistoricMicrApplication/src/main/java/ua/donetc/HistoryMicrApplication/entity/Question.java
+++ HistoricMicrApplication/src/main/java/ua/donetc/HistoryMicrApplication/entity/Question.java
@@ -2,7 +2,11 @@ package ua.donetc.HistoryMicrApplication.entity;
 
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
